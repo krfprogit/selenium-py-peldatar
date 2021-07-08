947275f2bdc56ff0763fa29ff87d9ee740ab5d62
krfprogit <krfprogit@gmail.com>
2021-07-08 02:23:58 +0200

    while-while-break-break

---
+++ python2-homework/fordito.py
@@ -0,0 +1,33 @@
+# 010 Feladat: Python lista gyakorlása
+# A feladatokat külön python fileban oldd meg. Minden feladat tartalmazza az elvárt filenevet.
+# Ezen a néven fogadható el a megoldás.
+#
+# . Írj egy Python programot, amely a felhasználótól pozitív számokat kér be mindaddig,
+# amíg a felhasználó nullát nem ad be! A program az összes értéket tárolja egy listában,
+# majd írja ki a képernyőre a lista elemeit fordított sorrendben!
+# A megoldást egy fordito.py nevű file-ban kell beadnod.
+#
+# Feladat beadása
+# A fent eméített python file-okat és benne a megoldásodat kérlek a saját selenium-py-peldatar github repo forkodban
+# egy python2-homework nevű mappába helyezd el.
+
+####################################################### MEGOLDAS
+
+print("A felhasználótól pozitív számokat kérek be mindaddig, amíg a felhasználó nullát nem ad be!")
+szamok = []
+while True:
+    while True:
+        try:
+            szam = float(input("Kérem adjon meg egy pozitív számot! "))
+            if szam <= 0:
+                break
+            szamok.append(szam)
+        except:
+            print("Csak pozitív számot fogadok el!")
+    if szam == 0:
+        break
+    print("Csak pozitív számot fogadok el!")
+
+szamok.reverse()
+print("A lista elemei fordított sorrendben:")
+print(szamok)
